cbad088a9275d9d5da99e2a6097b5f493e0d48bd
Gaetan Othenin-Girard <gaetan.othenin-girard@heig-vd.ch>
2017-06-11 21:05:28 +0200

    Correction bug.

--- docker-images/apache-php-image/content/assets/js/apod.js
+++ docker-images/apache-php-image/content/assets/js/apod.js
@@ -2,7 +2,7 @@ $(function() {
 	console.log("Loading APODs");
 
 	function loadAPOD() {
-		$.getJSON( "/api/apod/", function ( apod ) {
+		$.getJSON( "/api/students/", function ( apod ) {
 			console.log(apod);
 			var title = apod.title;
 			var src = apod.image;
