5d63af38aefc5d415a0765c7fb29e5e11180f66c
Gaetan Othenin-Girard <gaetan.othenin-girard@heig-vd.ch>
2017-06-11 17:17:29 +0200

    Modif étape 2 : on renvoie un JSON contenant une APOD random.

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -1,42 +1,12 @@
-var Chance = require('chance');
-var chance = new Chance();
-
 var express = require('express');
 var app = express();
 
+var randomApod = require('random-apod');
+
 app.get('/', function(req, res) {
-	res.send(generateStudent());
+	res.send(randomApod());
 });
 
 app.listen(6666, function() {
 	console.log('Acception HTTP requests on port 6666.');
 });
-
-function generateStudent() {
-	var numberOfStudents = chance.integer({
-		min: 0,
-		max:10
-	});
-	var studentList = [];
-	for (var i = 0; i < numberOfStudents; i++) {
-		var sexe = chance.gender();
-		var birthYear = chance.year({
-			min: 1990,
-			max: 1999
-		});
-		studentList.push({
-			firstName: chance.first({
-				genre: sexe,
-				nationality: "it"
-			}),
-			lastName: chance.last({
-				nationality: "it"
-			}),
-			sexe: sexe,
-			birthday: chance.birthday({
-				year: birthYear
-			})
-		});
-	}
-	return studentList;
-}
