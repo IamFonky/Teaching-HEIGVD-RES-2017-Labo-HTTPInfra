10b3101f4e7e89de3dc847b290ec2df6f3cb2268
P-B Monaco <pierre-benjamin.monaco@heig-vd.ch>
2017-06-11 18:02:38 +0200

    .gitignore file

--- docker-images/images-bash/step3.sh
+++ docker-images/images-bash/step3.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
+#Step 1 and 2 have to be executed before this step!
 
-# Execute the contener with apache server whitout mapping
+# go to reverse proxy image dir
+cd ../apache-reverse-proxy-image/content
+
+# Clean the conf file
+rm -f 001-default.conf
+cat 001-default.conf.head >> 001-default.conf
+
+# Execute the container with express server whitout mapping
+docker run -d --name express res/express
+
+# Get express ip
+express_ip=$(docker inspect express | grep -i "\"ipaddress\"" | tail -n 1 | cut -d "\"" -f4)
+
+# Write conf file for routing the express server
+echo ProxyPass "/api/students/" "http://" $express_ip ":3000/" >> 001-default.conf
+echo ProxyPassReverse "/api/students/" "http://" $express_ip ":3000/" >> 001-default.conf
+
+# Execute the container with apache server whitout mapping
 docker run -d --name apache_static res/apache-php
 
 # Get apache_static ip
 apache_static_ip=$(docker inspect apache_static | grep -i "\"ipaddress\"" | tail -n 1 | cut -d "\"" -f4)
 
-# Execute the contener with apache server whitout mapping
-docker run -d --name express res/apache-php
+# Write conf file for routing the express server
+echo ProxyPass "/" "http://" $apache_static_ip ":80/" >> 001-default.conf
+echo ProxyPassReverse "/" "http://" $apache_static_ip ":80/" >> 001-default.conf
 
-# Get apache_static ip
-express_ip=$(docker inspect apache_static | grep -i "\"ipaddress\"" | tail -n 1 | cut -d "\"" -f4)
-
-
-echo ProxyPass "/api/students/" "http://" $express_ip ":3000/" >> 001-default.conf
-echo ProxyPassReverse "/api/students/" "http://" $express_ip ":3000/" >> 001-default.conf
-
-
-#... run expressdocker
+# Close the conf file
+cat 001-default.conf.head >> 001-default.conf
 
 # Build the reverse-proxy image (warning, it has to be executed in the image directory)
 docker build -t res/reverse-proxy ../apache-reverse-proxy-image
