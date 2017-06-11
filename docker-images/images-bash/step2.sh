d6834902d8686fd7487b2ff1ee34fca9866ecf2b
Gaetan Othenin-Girard <gaetan.othenin-girard@heig-vd.ch>
2017-06-11 15:16:44 +0200

    Etape 2a done.

---
+++ docker-images/images-bash/step2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Build the docker image (warning, it has to be executed in the image directory)
+docker build -t res/express ../express-image
+
+# Execute the contener and map the local 9090 port on the 80 port of the contener
+sudo docker run res/express
