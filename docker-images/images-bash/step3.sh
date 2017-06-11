426b4334fb083ba12a1a7ca5619aeca944ef42d7
P-B Monaco <pierre-benjamin.monaco@heig-vd.ch>
2017-06-11 18:51:44 +0200

    Etape 3 devrait marcher

--- docker-images/images-bash/step3.sh
+++ docker-images/images-bash/step3.sh
@@ -16,8 +16,8 @@ docker run -d --name express res/express
 express_ip=$(docker inspect express | grep -i "\"ipaddress\"" | tail -n 1 | cut -d "\"" -f4)
 
 # Write conf file for routing the express server
-echo ProxyPass "/api/students/" "http://" $express_ip ":3000/" >> 001-default.conf
-echo ProxyPassReverse "/api/students/" "http://" $express_ip ":3000/" >> 001-default.conf
+echo ProxyPass "\"/api/students/\"" "\"http://$express_ip:3000/\"" >> 001-default.conf
+echo ProxyPassReverse "\"/api/students/\"" "\"http://$express_ip:3000/\"" >> 001-default.conf
 
 # Execute the container with apache server whitout mapping
 docker run -d --name apache_static res/apache-php
@@ -26,18 +26,20 @@ docker run -d --name apache_static res/apache-php
 apache_static_ip=$(docker inspect apache_static | grep -i "\"ipaddress\"" | tail -n 1 | cut -d "\"" -f4)
 
 # Write conf file for routing the express server
-echo ProxyPass "/" "http://" $apache_static_ip ":80/" >> 001-default.conf
-echo ProxyPassReverse "/" "http://" $apache_static_ip ":80/" >> 001-default.conf
+echo ProxyPass "\"/\"" "\"http://$apache_static_ip:80/\"" >> 001-default.conf
+echo ProxyPassReverse "\"/\"" "\"http://$apache_static_ip:80/\"" >> 001-default.conf
 
 # Close the conf file
-cat 001-default.conf.head >> 001-default.conf
+cat 001-default.conf.foot >> 001-default.conf
+
+# Get back to reverse proxy image dir
+cd ..
 
 # Build the reverse-proxy image (warning, it has to be executed in the image directory)
-docker build -t res/reverse-proxy ../apache-reverse-proxy-image
+docker build -t res/reverse-proxy .
 
-# Execute the contener with apache server whitout mapping
-docker run -it -d --name apache_proxy res/reverse-proxy
+# Execute the reverse proxy container
+docker run -p 8080:80 res/reverse-proxy
 
 
-docker run -it
--p 8080:80 php:5.6-apache /bin/bash
+#docker run -it php:5.6-apache /bin/bash
