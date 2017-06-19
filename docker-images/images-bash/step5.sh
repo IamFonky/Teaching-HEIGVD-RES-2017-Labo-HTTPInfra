ce08b1ad4e2049e5c7fedabff490462709785a2b
P-B Monaco <pierre-benjamin.monaco@heig-vd.ch>
2017-06-19 09:20:39 +0200

    Testing step5

--- docker-images/images-bash/step5.sh
+++ docker-images/images-bash/step5.sh
@@ -5,42 +5,14 @@
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
 
-# go to reverse proxy image dir
-#cd ../apache-reverse-proxy-image/content
-
-## Clean the conf file
-#rm -f 001-default.conf
-#cat HEAD.001-default.conf >> 001-default.conf
-#
 ## Execute the container with express server whitout mapping
-#docker build -t res/express ../../express-image
 docker run -d --name express res/express
-#
-## Get express ip
-#express_ip=$(docker inspect express | grep -i "\"ipaddress\"" | tail -n 1 | cut -d "\"" -f4)
-#
-## Write conf file for routing the express server
-#echo ProxyPass "\"/api/students/\"" "\"http://$express_ip:6666/\"" >> 001-default.conf
-#echo ProxyPassReverse "\"/api/students/\"" "\"http://$express_ip:6666/\"" >> 001-default.conf
-#
-## Execute the container with apache server whitout mapping
-#docker build -t res/apache-php ../../apache-php-image
-docker run -d --name apache_static res/apache-php
-#
-## Get apache_static ip
-#apache_static_ip=$(docker inspect apache_static | grep -i "\"ipaddress\"" | tail -n 1 | cut -d "\"" -f4)
-#
-## Write conf file for routing the express server
-#echo ProxyPass "\"/\"" "\"http://$apache_static_ip:80/\"" >> 001-default.conf
-#echo ProxyPassReverse "\"/\"" "\"http://$apache_static_ip:80/\"" >> 001-default.conf
-#
-## Close the conf file
-#cat FOOT.001-default.conf >> 001-default.conf
 
-# Get back to reverse proxy image dir
-#cd ..
+## Execute the container with apache server whitout mapping
+docker run -d --name apache_static res/apache-php
 
 # Build the reverse-proxy image (warning, it has to be executed in the image directory)
+chmod 755 ../apache-reverse-proxy-image-dynamic/apache2-forground
 docker build -t res/reverse-proxy-2 ../apache-reverse-proxy-image-dynamic
 
 # Execute the reverse proxy container
